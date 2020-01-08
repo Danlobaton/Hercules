44be85efbf8351624888a20da806a0dc5cf6d36e
elopez00 <ejlopez00@gmail.com>
2020-01-08 18:32:25 -0500

    hello

--- client/src/components/functional/FacebookLogin.js
+++ client/src/components/functional/FacebookLogin.js
@@ -77,6 +77,7 @@ export default class FacebookLogin extends Component {
 
   render() {
     let {children} = this.props;
+    console.log(window.FB)
     return (
       <div onClick={this.facebookLogin} >
         {children}
