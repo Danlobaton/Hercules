a3c24ca26206ca1ebe29d4e4d289c8140da100b5
adm-eddie <eduardo@justadm.com>
2020-02-06 13:33:22 -0500

    fixed build issue

--- client/src/components/LoadingState.js
+++ client/src/components/LoadingState.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import InitLoading from './pages/initLoading'
+import InitLoading from './pages/InitLoading'
 
 export class LoadingState extends Component {
     render() {
